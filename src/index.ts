f2768349a8eb9da1f14963bd41422945f752e9bb
SpacingBat3 <git@spacingbat3.anonaddy.com>
2022-07-13 01:21:38 +0200

    Fix `.npmignore` and ESM types.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import lib, {colors as color, modifiers as mod} from "./index.cjs";
 export const colors = color;
 export const modifiers = mod;
 // Removes duplicate '.default' property.
-export default (lib as unknown as {default: typeof lib}).default;
+export default lib.default
