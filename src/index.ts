0653b33992031b66184ae189309f786dad316ade
SpacingBat3 <git@spacingbat3.anonaddy.com>
2025-01-05 17:18:40 +0100

    Refactors and cosmetic changes.
    • Initialize all arrays as immutable.
    • Make `Dict` type closer to the actual type of the objects that are
      compared to it.
    • Remove some whitespace characters at the end of the lines.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-type Dict = Record<string,readonly [number,number]>
+type Dict = Readonly<Record<string,readonly [number,number]>>
 
 type looseString = string|number|bigint|boolean|null|undefined
 
@@ -31,57 +31,57 @@ type dictMap<T extends Dict> = {
 }
 
 const modifiers_safe = Object.freeze({
-  reset:     [0,  0],
-  bold:      [1, 22],
-  underline: [4, 24],
-  inverse:   [7, 27]
+  reset:     Object.freeze(<const>[0,  0]),
+  bold:      Object.freeze(<const>[1, 22]),
+  underline: Object.freeze(<const>[4, 24]),
+  inverse:   Object.freeze(<const>[7, 27])
 } as const) satisfies Dict;
 
 const modifiers_other = Object.freeze({
-  dim:             [ 2, 22],
-  italic:          [ 3, 23],
-  blink:           [ 5, 25],
-  rapidBlink:      [ 6, 25],
-  hidden:          [ 8, 28],
-  strikethrough:   [ 9, 29],
-  doubleunderline: [21, 24],
-  framed:          [51, 54],
-  overlined:       [53, 55]
+  dim:             Object.freeze(<const>[ 2, 22]),
+  italic:          Object.freeze(<const>[ 3, 23]),
+  blink:           Object.freeze(<const>[ 5, 25]),
+  rapidBlink:      Object.freeze(<const>[ 6, 25]),
+  hidden:          Object.freeze(<const>[ 8, 28]),
+  strikethrough:   Object.freeze(<const>[ 9, 29]),
+  doubleunderline: Object.freeze(<const>[21, 24]),
+  framed:          Object.freeze(<const>[51, 54]),
+  overlined:       Object.freeze(<const>[53, 55])
 } as const) satisfies Dict;
 
 const colors_util = Object.freeze({
-  black:           [ 30, 39],
-  red:             [ 31, 39],
-  green:           [ 32, 39],
-  yellow:          [ 33, 39],
-  blue:            [ 34, 39],
-  magenta:         [ 35, 39],
-  cyan:            [ 36, 39],
-  white:           [ 37, 39],
-  bgBlack:         [ 40, 49],
-  bgRed:           [ 41, 49],
-  bgGreen:         [ 42, 49],
-  bgYellow:        [ 43, 49],
-  bgBlue:          [ 44, 49],
-  bgMagenta:       [ 45, 49],
-  bgCyan:          [ 46, 49],
-  bgWhite:         [ 47, 49],
-  gray:            [ 90, 39],
-  redBright:       [ 91, 39],
-  greenBright:     [ 92, 39],
-  yellowBright:    [ 93, 39],
-  blueBright:      [ 94, 39],
-  magentaBright:   [ 95, 39],
-  cyanBright:      [ 96, 39],
-  whiteBright:     [ 97, 39],
-  bgGray:          [100, 49],
-  bgRedBright:     [101, 49],
-  bgGreenBright:   [102, 49],
-  bgYellowBright:  [103, 49],
-  bgBlueBright:    [104, 49],
-  bgMagentaBright: [105, 49],
-  bgCyanBright:    [106, 49],
-  bgWhiteBright:   [107, 49]
+  black:           Object.freeze(<const>[ 30, 39]),
+  red:             Object.freeze(<const>[ 31, 39]),
+  green:           Object.freeze(<const>[ 32, 39]),
+  yellow:          Object.freeze(<const>[ 33, 39]),
+  blue:            Object.freeze(<const>[ 34, 39]),
+  magenta:         Object.freeze(<const>[ 35, 39]),
+  cyan:            Object.freeze(<const>[ 36, 39]),
+  white:           Object.freeze(<const>[ 37, 39]),
+  bgBlack:         Object.freeze(<const>[ 40, 49]),
+  bgRed:           Object.freeze(<const>[ 41, 49]),
+  bgGreen:         Object.freeze(<const>[ 42, 49]),
+  bgYellow:        Object.freeze(<const>[ 43, 49]),
+  bgBlue:          Object.freeze(<const>[ 44, 49]),
+  bgMagenta:       Object.freeze(<const>[ 45, 49]),
+  bgCyan:          Object.freeze(<const>[ 46, 49]),
+  bgWhite:         Object.freeze(<const>[ 47, 49]),
+  gray:            Object.freeze(<const>[ 90, 39]),
+  redBright:       Object.freeze(<const>[ 91, 39]),
+  greenBright:     Object.freeze(<const>[ 92, 39]),
+  yellowBright:    Object.freeze(<const>[ 93, 39]),
+  blueBright:      Object.freeze(<const>[ 94, 39]),
+  magentaBright:   Object.freeze(<const>[ 95, 39]),
+  cyanBright:      Object.freeze(<const>[ 96, 39]),
+  whiteBright:     Object.freeze(<const>[ 97, 39]),
+  bgGray:          Object.freeze(<const>[100, 49]),
+  bgRedBright:     Object.freeze(<const>[101, 49]),
+  bgGreenBright:   Object.freeze(<const>[102, 49]),
+  bgYellowBright:  Object.freeze(<const>[103, 49]),
+  bgBlueBright:    Object.freeze(<const>[104, 49]),
+  bgMagentaBright: Object.freeze(<const>[105, 49]),
+  bgCyanBright:    Object.freeze(<const>[106, 49]),
+  bgWhiteBright:   Object.freeze(<const>[107, 49])
 } as const) satisfies Dict;
 
 const shouldUseColors = (() => {
@@ -137,9 +137,9 @@ function alias<T extends object,R extends object>(value:T, hook:(value:T)=>R) {
   });
 }
 
-/** 
+/**
  * An object containing multiple functions used to colorize the text.
- * 
+ *
  * @example
  * // Print error in the console.
  * console.error(colors.red("[Error]")+" Something's not right...")
@@ -202,7 +202,7 @@ const defaultExport = Object.freeze({
   unsafe: modifiers.other
 });
 
-export { 
+export {
   colors,
   modifiers
 }
